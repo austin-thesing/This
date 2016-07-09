1a510b896c624047362b420299952c5e49ba389d
Austin Thesing <admin@designxdevelop.com>
2016-07-09 13:25:53 -0700

    Basic Articles controller/views || Added articles count on user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701022244) do
+ActiveRecord::Schema.define(version: 20160709200427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "url"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -40,5 +48,6 @@ ActiveRecord::Schema.define(version: 20160701022244) do
     t.index ["user_id"], name: "index_users_on_user_id", using: :btree
   end
 
+  add_foreign_key "articles", "users"
   add_foreign_key "users", "users"
 end
